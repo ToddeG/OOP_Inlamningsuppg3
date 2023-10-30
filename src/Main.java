7ab3e347f79a581923471cb1b723961708e83262
Daniel Jägestedt <daniel_jege@hotmail.com>
2023-10-30 16:22:32 +0100

    Puzzle Class first attempt

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
-    public static void main(String[] args) {
 
-        //Hej
+    public static void main(String[] args) {
+        FifteenPuzzle fp = new FifteenPuzzle();
+
+
     }
 }
